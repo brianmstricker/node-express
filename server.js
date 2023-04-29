c97c19beed397d22fae134e1ba097557b3414034
Brian Stricker <brianmstricker@gmail.com>
2023-04-29 11:30:42 -0400

    wk1/updated campsite/partner/promotion

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
-const campsiteRouter = require("./routes/campsiteRouter");
 const hostname = "localhost";
 const port = 3000;
 const app = express();
 app.use(morgan("dev"));
 app.use(express.json());
+const campsiteRouter = require("./routes/campsiteRouter");
 app.use("/campsites", campsiteRouter);
+const promotionRouter = require("./routes/promotionRouter");
+app.use("/promotions", promotionRouter);
+const partnerRouter = require("./routes/partnerRouter");
+app.use("/partners", partnerRouter);
 app.use(express.static(__dirname + "/public"));
 app.use((req, res) => {
   res.statusCode = 200;
